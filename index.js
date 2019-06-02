aa8101f568045b5c978f4df1f7e019ce65d7a910
Mehmet Fatih Yıldız <fatih@mfyz.com>
2019-06-01 21:27:57 -0700

    view update

--- index.js
+++ index.js
@@ -147,9 +147,7 @@ app.get('/repositories', authRequired, (req, res) => {
 		headers: { "Authorization": `token ${req.user.githubtoken}` }
 	})
 		.then((response) => {
-			// console.log('github api call respnse', response)
-			res.send(JSON.stringify(response.data, null, 4))
-			// res.render('respositories', { respositories: response.data })
+			res.render('respositories', { respositories: response.data })
 		})
 		.catch((err) => {
 			console.log('github api call returned with err', err)
