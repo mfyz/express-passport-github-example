25f31677d17512961ddf9ea34a743398f4020673
Mehmet Fatih Yıldız <fatih@mfyz.com>
2019-06-01 21:23:54 -0700

    view update

--- index.js
+++ index.js
@@ -148,7 +148,7 @@ app.get('/repositories', authRequired, (req, res) => {
 	})
 		.then((response) => {
 			// console.log('github api call respnse', response)
-			res.send(response)
+			res.send(JSON.stringify(response, null, 4))
 			// res.render('respositories', { respositories: response.data })
 		})
 		.catch((err) => {
